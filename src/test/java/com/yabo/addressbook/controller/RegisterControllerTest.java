747b39476ac4a58a905e2ff3ac54d0043d6b5b5c
voidbytes <13680422+voidbytes@users.noreply.github.com>
2026-07-04 14:32:12 +0800

    test: fix and supplement unit tests - all 100 pass
    - ValidationUtilTest: update for new username regex (letters+digits, start with letter)
    - ContactControllerTest: update URLs to /api/v1/, remove Thymeleaf view tests, add missing MockBeans
    - RegisterControllerTest: update to REST style, add @WithMockUser
    - UserServiceTest: add coverage for nickname, updateProfile, updatePassword, generateRandomNickname
    - AdminServiceTest: add coverage for username/nickname update, resetPassword, fix password length

--- src/test/java/com/yabo/addressbook/controller/RegisterControllerTest.java
+++ src/test/java/com/yabo/addressbook/controller/RegisterControllerTest.java
@@ -1,11 +1,13 @@
 package com.yabo.addressbook.controller;
 
 import com.yabo.addressbook.service.CaptchaService;
+import com.yabo.addressbook.service.LoginAttemptService;
 import com.yabo.addressbook.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
@@ -24,7 +26,11 @@ class RegisterControllerTest {
     @MockBean
     private CaptchaService captchaService;
 
+    @MockBean
+    private LoginAttemptService loginAttemptService;
+
     @Test
+    @WithMockUser
     void checkUsername_shouldReturnBoolean() throws Exception {
         when(userService.existsByUsername("testuser")).thenReturn(false);
 
